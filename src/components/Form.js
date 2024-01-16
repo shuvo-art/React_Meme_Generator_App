cf550f5abde011ddf30c5fbd66796284e5c92321
shuvo-art <shuvo83qn@gmail.com>
2024-01-16 21:07:54 +0600

    Form Handling in a formal way

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,21 +6,29 @@ export default function Form() {
             firstName: "",
             lastName: "",
             email: "",
-            textArea: ""
+            textArea: "",
+            checkbox: false,
+            radio: "",
+            favFruit: ""
         }
     )
 
     function handleChange(event) {
+        const {name, value, type, checked} = event.target;
         setFormData(prevFormData => ({
             ...prevFormData,
-            [event.target.name]: event.target.value
+            [name]: type === "checkbox" ? checked : value
         }));
     }
-    console.log(formData);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(formData)
+    }
 
     return (
         <div className="form">
-            <form action="" method="POST" id="my-form">
+            <form onSubmit={handleSubmit} id="my-form">
                 <label htmlFor="first-name">First Name:</label>
                 <input onChange={handleChange} value={formData.firstName} type="text" id="first-name" name="firstName" placeholder="First Name" />
                 <br />
@@ -31,7 +39,44 @@ export default function Form() {
                 <input onChange={handleChange} value={formData.email} type="email" id="email" name="email" placeholder="Email" />
                 <br />
                 <textarea onChange={handleChange}  value={formData.textArea} name="textArea" id="text-area" className="textarea" cols="30" rows="10" placeholder="Text Area" />
-                <input type="submit" value="Submit" />
+                <br />
+                <input onChange={handleChange} value={formData.checkbox} type="checkbox" name="checkbox" id="checkbox" />
+                <label htmlFor="checkbox">Is this Friedly ?</label>
+                <br />
+                <fieldset>
+                    <legend>Current employment status</legend>
+                    <input onChange={handleChange} value="Employed" checked={formData.radio === "Employed"} type="radio" name="radio" id="employed" />
+                    <label htmlFor="employed">Employed</label>
+                    <br />
+                    <input onChange={handleChange} value="Unemployed" checked={formData.radio === "Unemployed"} type="radio" name="radio" id="unemployed" />
+                    <label htmlFor="unemployed">Unemployed</label>
+                    <br />
+                    <input 
+                        type="radio" 
+                        id="part-time" 
+                        name="radio" 
+                        value="Part-Time" 
+                        checked={formData.radio === "Part-Time"} 
+                        onChange={handleChange}                     //Standard format of form handling
+                    />
+                    <label htmlFor="part-time">Part-Time</label>
+                </fieldset>
+                <br />
+                <label htmlFor="fav-fruit">Choose a fruit:</label>
+                        <select 
+                            id="fav-fruit"
+                            name="favFruit"
+                            value={formData.favFruit}
+                            onChange={handleChange} 
+                        >
+                            <option value="">--Chose Fruit--</option>    
+                            <option value="apple">Apple</option>
+                            <option value="banana">Banana</option>
+                            <option value="orange">Orange</option>
+                            <option value="grape">Grape</option>
+                        </select>
+                <br />
+                <button>Submit</button>
             </form>
         </div>
     )
